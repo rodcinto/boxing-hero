7935dda3cc9a7f4d055a6f7259a7763bcf70eb0b
Rodrigo Cinto <rodcinto@gmail.com>
2022-12-04 12:05:12 +0100

    First structure, music file and button.

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    function buttonClickedHandler() {
+        console.log('PLAY PUSHED!');
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.text}>
+                Become a Boxing Hero!
+            </Text>
+
+            <TouchableOpacity
+                onPress={buttonClickedHandler}
+                style={styles.roundButton}>
+                <Text>PLAY</Text>
+            </TouchableOpacity>
+
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    content: {
+    },
+    text: {
+        color: '#fff',
+        fontWeight: 'bold',
+        padding: 10
+    },
+    roundButton: {
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 100,
+        backgroundColor: '#A00',
+        color: '#fff',
+        fontWeight: 'bold',
+    },
 });
